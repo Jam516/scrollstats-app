262306f35d8648b79ebf3dd9a2f4716b9fa7d53e
Jam516 <kufuorkofi@gmail.com>
2024-02-23 14:33:02 +0000

    commit

--- components/schema.ts
+++ components/schema.ts
@@ -10,7 +10,7 @@ export const appSchema = z.object({
     TRANSACTIONS_GROWTH: z.string(),
     WALLETS: z.number(),
     WALLETS_GROWTH: z.string(),
-    TVL: z.number().optional(),
+    TVL: z.number().nullable().optional(),
     CATEGORY: z.string(),
 })
 
